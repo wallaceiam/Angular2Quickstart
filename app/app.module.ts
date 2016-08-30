f3c6c0fb892050d2af89e4a86b8afaba2ffb1065
Chris Wallace <wallacec@financialsoftware.co.uk>
2016-08-30 17:54:11 +0100

    Completed QuickStart, Tutorial 1-5 Startiing on Tutorial 6

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { routing }      from './app.routing';
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, routing ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  providers:    [ HeroService ] ,
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
